7f15f99fc3e2291c27017669b9fd4d08c85e5466
Varvara Korableva <v.korableva1990@yandex.ru>
2024-02-04 09:59:49 +0200

    add env

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 
 app.get("/api/:name", (req, res) => {
-  res.json({ message: `Hello ${req.params.name}, from server!` });
+  res.json({ message: `Hello ${req.params.name}, from server! MY_NAME => ${process.env.MY_NAME}` });
 });
 
 app.listen(process.env.PORT || 3001, () => {
